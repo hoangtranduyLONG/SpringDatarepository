7f3b77b8b43a191e7b4b2bcc0ecd1e5bc5531ced
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-06-16 10:14:48 +0700

    done

---
+++ src/main/java/controller/ProvinceController.java
@@ -0,0 +1,57 @@
+package controller;
+
+import model.Province;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import service.IProvinceService;
+
+@Controller
+@RequestMapping("/provinces")
+public class ProvinceController {
+    @Autowired
+    IProvinceService provinceService;
+
+    @GetMapping
+    public ModelAndView show() {
+        ModelAndView modelAndView = new ModelAndView("/province/list");
+        modelAndView.addObject("provinces", provinceService.findAll());
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/province/create");
+        return modelAndView;
+    }
+
+    @PostMapping("/create")
+    public ModelAndView save(Province province) {
+        provinceService.save(province);
+        ModelAndView modelAndView = new ModelAndView("redirect:/provinces");
+        return modelAndView;
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("/province/edit");
+        Province province = provinceService.findById(id).get();
+        modelAndView.addObject("pro", province);
+        return modelAndView;
+    }
+
+    @PostMapping("/edit/{id}")
+    public ModelAndView update(@PathVariable Long id, Province province) {
+        ModelAndView modelAndView = new ModelAndView("redirect:/provinces");
+//        Province oldProvince = provinceService.findById(id).get();
+//        oldProvince.setDescription(province.getDescription());
+//        oldProvince.setName(province.getName());
+        provinceService.save(province);
+        return modelAndView;
+    }
+}
